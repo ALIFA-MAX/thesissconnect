b63370d940be4c92c64cb27f35db23335b13c474
ALIFA-MAX <23-50170-1@student.aiub.edu>
2026-01-21 23:26:15 +0600

    database

--- thesisconnect.sql
+++ thesisconnect.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 21, 2026 at 11:16 AM
+-- Generation Time: Jan 21, 2026 at 06:24 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `thesisconnect`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requests`
+--
+
+CREATE TABLE `requests` (
+  `student_id` varchar(50) NOT NULL,
+  `teacher_id` varchar(50) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `requests`
+--
+
+INSERT INTO `requests` (`student_id`, `teacher_id`, `status`) VALUES
+('23', '111', 'accepted');
 
 -- --------------------------------------------------------
 
@@ -71,16 +90,19 @@ CREATE TABLE `user_data` (
   `full_name` varchar(50) NOT NULL,
   `teacher_id` varchar(50) NOT NULL,
   `designation` varchar(50) NOT NULL,
-  `research_fields` varchar(50) NOT NULL
+  `research_fields` varchar(500) NOT NULL,
+  `status` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user_data`
 --
 
-INSERT INTO `user_data` (`role`, `email`, `phone`, `department`, `student_id`, `major`, `semester`, `batch`, `cgpa`, `password`, `full_name`, `teacher_id`, `designation`, `research_fields`) VALUES
-('professor', '23-50170-1@student.aiub.edu', '01408661776', 'Computer Science', '', '', '', '', '', '111111', 'FATIMA', '111', 'Professor', '11'),
-('professor', '23-50170-1@student.aiub.edu', '01408661776', 'Computer Science', '', '', '', '', '', '1111', 'FATIMA', '001', 'Associate Professor', '11');
+INSERT INTO `user_data` (`role`, `email`, `phone`, `department`, `student_id`, `major`, `semester`, `batch`, `cgpa`, `password`, `full_name`, `teacher_id`, `designation`, `research_fields`, `status`) VALUES
+('professor', '23-50170-1@student.aiub.edu', '01408661776', 'Computer Science', '', '', '', '', '', '111111', 'Fatima', '111', 'Professor', 'naswdasd sa', ''),
+('student', 'mubin9516@gmail.com', '0187050065', 'Electrical Engineering', '23', 'dsadasdas', '10', '2021', '3.5', '111111', 'Abdullah al mubin', '', '', '', 'Not currently seeking'),
+('professor', '23-50170-1@student.aiub.edu', '01408661776', 'Computer Science', '', '', '', '', '', '11111', 'Fatima', '111', 'Professor', 'naswdasd sa', ''),
+('professor', '23-50170-1@student.aiub.edu', '01408661776', 'Computer Science', '', '', '', '', '', '1111', 'Fatima', '111', 'Professor', 'naswdasd sa', '');
 
 --
 -- Indexes for dumped tables
